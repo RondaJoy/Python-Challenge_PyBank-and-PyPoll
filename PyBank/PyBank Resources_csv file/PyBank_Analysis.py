446b38c904562f6c8dfb6deb7a2ca0f34f81a1f8
Ronda Hinz (PC) <130201073+RondaJoy@users.noreply.github.com>
2023-05-02 21:54:34 -0700

    Next Save_V3

--- PyBank/PyBank Resources_csv file/PyBank_Analysis.py
+++ PyBank/PyBank Resources_csv file/PyBank_Analysis.py
@@ -1,13 +1,24 @@
 import csv
 
-with open('budget_data.csv', newline='') as BudgetData:
-    BudgetRead = csv.DictReader(BudgetData)
+BudgetData = open('budget_data.csv', 'r')
+BudgetRead = csv.DictReader(BudgetData)
 rowdata = list(BudgetRead)
 RowNum = len(rowdata)
 print("Total Months: " +str(RowNum))
 Tot_Profit_Loss = 0
-for col in BudgetRead:
+for col in rowdata:
     Tot_Profit_Loss += int(col['Profit/Losses'])
 
 print("Total: $" + str(Tot_Profit_Loss))
-    
+
+BudgetData = open('budget_data.csv', 'r')
+BudgetRead = csv.DictReader(BudgetData)
+PLCol = [float(row['Profit/Losses']) for row in rowdata]
+delta = []
+for x in range(len(PLCol) -1):
+    Diffs = PLCol[x+1] - PLCol[x]
+    delta.append(Diffs)
+
+avg_Diffs = sum(delta) / len(delta)
+
+print("Average Change: $" + str(round(avg_Diffs, 2)))
