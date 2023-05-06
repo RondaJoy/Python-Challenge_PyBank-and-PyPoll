25c21d00473319ba4182565ac77c45e9905e7add
Ronda Hinz (PC) <130201073+RondaJoy@users.noreply.github.com>
2023-05-05 17:48:43 -0700

    Add Write Commands

---
+++ PyPoll/PyPoll Resources_csv file/Test for PyPoll.py
@@ -0,0 +1,67 @@
+import csv
+import os
+
+# read data from .csv file
+with open(os.path.abspath(r"C:\Users\ronda\Documents\Bootcamp\GitHub\Python-Challenge\PyPoll\Resources\election_data.csv"), 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # skip header
+    PollData = list(reader)
+
+# tally the number of rows/votes
+TotVotes = len(PollData)
+
+# open the text file for writing
+with open("Poll_Results.txt", "w") as file:
+
+    # write the headers and first two lines to the text file
+    file.write("Election Results\n")
+    file.write("-------------------------\n")
+    file.write("\n")
+    file.write(f"Total Votes: {TotVotes}\n")
+    file.write("-------------------------\n")
+
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {TotVotes}")
+    print("-------------------------")
+
+    # create a list to store tally for each candidate
+    vcount = {}
+
+    # begin for loop to count occurrences of candidate name in Column C/2
+    for col in PollData:
+        CAN = col[2]
+        if CAN in vcount:
+            vcount[CAN] += 1
+        else:
+            vcount[CAN] = 1
+
+    TCount = []
+    CCount = []
+
+    # generate output for candidate name, % of total vote, and actual # of votes
+    for CAN, count in vcount.items():
+        PCNT = (count / TotVotes) * 100
+        format_PCNT = round(PCNT, 3)
+        print(f"{CAN}: {format_PCNT}% ({count})")
+        file.write(f"{CAN}: {format_PCNT}% ({count})\n")
+
+        TCount.append(count)
+        CCount.append(CAN)
+
+    print("-------------------------")
+    print()
+
+    file.write("-------------------------\n")
+    file.write("\n")
+
+    WinCount = max(TCount)
+    result = []
+    index = []
+    for x, item in enumerate(TCount):
+        if item == WinCount:
+            result.append(item)
+            index.append(x)
+    x = index[0]
+    print(f"Winner: {CCount[x]}")
+    file.write(f"Winner: {CCount[x]}\n")
